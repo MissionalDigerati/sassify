1c5d0859a5057acfaea1c49cbb3e44333e6f51fb
Codemis <johnathan@missionaldigerati.com>
2014-07-10 10:56:50 -0700

    Added test to verify the script replaces paths with no parentheses and with a leading path.

--- spec/lib/sassify_spec.rb
+++ spec/lib/sassify_spec.rb
@@ -33,4 +33,19 @@ describe "Sassify" do
     expect(actual.delete!("\n")).to eq(expected_content)
   end
 
+  it "should generate the correct syntax if missing parentheses and has a relative pathing" do
+    expected_content = 'div.button span {'
+    expected_content += 'background-image: asset-url("test/uniform/sprite.png");'
+    expected_content += '}'
+    expected_content += 'div.button span.tester {'
+    expected_content += 'background-image: asset-url("test/uniform/tester.png");'
+    expected_content += '}'
+    test_file = File.join(@test_css_dir, 'no_quotes_with_relative_path.css')
+    completed_file = File.join(@test_css_dir, 'no_quotes_with_relative_path.css.scss')
+
+    system("#{@bash_script} img test #{test_file}")
+    actual = File.read(completed_file)
+    expect(actual.delete!("\n")).to eq(expected_content)
+  end
+
 end
